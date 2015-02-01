e88a2742de9d33e542ed2f8d0e4e2ba379e72850
jakobmaier <jakobmaier42@gmail.com>
2015-02-01 15:40:20 +0100

    Issue #24: Updated gitignore with new project name

--- Tablify/TypeDefinitions.js
+++
@@ -1,9 +0,0 @@
-var Tablify;
-(function (Tablify) {
-    (function (RowType) {
-        RowType[RowType["title"] = 0] = "title";
-        RowType[RowType["body"] = 1] = "body";
-    })(Tablify.RowType || (Tablify.RowType = {}));
-    var RowType = Tablify.RowType;
-    ;
-})(Tablify || (Tablify = {}));
